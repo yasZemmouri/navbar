0618b64cb958982f86e594601ba92be08c343fe7
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-11-02 23:20:00 -0400

    activate hamburger button:

---
+++ logic/myjs.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const toggleButton = document.getElementById('toggleButton');
+const naviList = document.getElementById('navi-list');
+
+toggleButton.addEventListener('click', () => {
+    naviList.classList.toggle('active');
+})
